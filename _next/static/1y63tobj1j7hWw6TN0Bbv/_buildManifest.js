76351073ee91dbc4f1d0db6e94caeebceff95de0
vikyw89 <vikyw89@users.noreply.github.com>
2023-04-14 11:08:16 +0000

    deploy: 841c6097bb29ba4babb874fdf62be7fae09b5351

---
+++ _next/static/1y63tobj1j7hWw6TN0Bbv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/be8208eff0a6dc03.css","static/chunks/pages/index-effd11ec70476760.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/MmcaSRG5mg0lmJfHVeiz6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a6086129adba8575.css","static/chunks/pages/index-94a8508e915676b7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
