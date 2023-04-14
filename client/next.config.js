4818287826f8a0b26cc0934ed6aa193a293def6b
vikyw <vikyw89@gmail.com>
2023-04-14 16:35:54 +0800

    new: added lint fix

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash:true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 }
 
 module.exports = nextConfig
