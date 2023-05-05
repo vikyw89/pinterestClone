45304834b6f163d47c2a78e228faf442cee54a54
vikyw89 <vikyw89@gmail.com>
2023-05-05 16:34:31 +0800

    removed client side db set up

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -40,15 +40,6 @@ updateSyncV(
   ].sort((a, b) => (a > b ? 1 : -1))
 )
 
-const isNewUser = async (user_id) => {
-  const userFromDB = await supabase
-    .from('users')
-    .select()
-    .eq('id', user_id)
-  const result = userFromDB.data.length === 0
-  return result
-}
-
 export default function App({ Component, pageProps }) {
   const activeTheme = useSyncV('activeTheme')
   const auth = useAsyncV('auth', { initialState: { loading: true } })
@@ -57,22 +48,6 @@ export default function App({ Component, pageProps }) {
   useEffect(() => {
     document.querySelector('html').setAttribute('data-theme', activeTheme)
   })
-
-  useEffect(() => {
-    if (!auth.data || auth.loading) return
-    // check if there's user_id in DB
-    const prepareNewUserDB = async () => {
-      if (!await isNewUser(auth.data.user.id)) return
-      setAsyncV('initialize', async () => {
-        const response = await supabase
-          .rpc('initialize', { id: auth.data.user.id, profile_picture_url: auth.data.user.user_metadata.picture })
-          .throwOnError()
-        return response
-      })
-    }
-    prepareNewUserDB()
-    // prepopulate new user DB
-  }, [auth.data])
 
   useEffect(() => {
     const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
