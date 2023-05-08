f5618e00e6779ad368318fa45b5b5cb7654f89a7
vikyw89 <vikyw89@gmail.com>
2023-05-09 00:37:14 +0800

    updated nextconfig

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: `${process.env.NEXT_PUBLIC_BASE_PATH}/`,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
-  trailingSlash:true,
-  basePath:process.env.NEXT_PUBLIC_BASE_PATH
+  eslint: {
+    dirs: ['src']
+  }
 }
 
 
