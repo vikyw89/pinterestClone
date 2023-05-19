24a2adc2477634e2608c51d0ee3bf9ed326f43a3
vikyw89 <vikyw89@gmail.com>
2023-05-19 15:01:50 +0800

    lint:fix

--- client/src/lib/hooks/useAvailableTheme.js
+++ client/src/lib/hooks/useAvailableTheme.js
@@ -1,38 +1,41 @@
+import { useEffect } from 'react'
 import { setSyncSWR, useSyncSWR } from 'swr-sync-state'
 
 export const useAvailableTheme = () => {
   const theme = useSyncSWR('theme')
-
-  setSyncSWR('theme', [
-    'light',
-    'dark',
-    'cupcake',
-    'bumblebee',
-    'emerald',
-    'corporate',
-    'synthwave',
-    'retro',
-    'cyberpunk',
-    'valentine',
-    'halloween',
-    'garden',
-    'forest',
-    'aqua',
-    'lofi',
-    'pastel',
-    'fantasy',
-    'wireframe',
-    'black',
-    'luxury',
-    'dracula',
-    'cmyk',
-    'autumn',
-    'business',
-    'acid',
-    'lemonade',
-    'night',
-    'coffee',
-    'winter',
-  ].sort((a, b) => (a > b ? 1 : -1)))
+  useEffect(() => {
+    setSyncSWR('theme', [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+    ].sort((a, b) => (a > b ? 1 : -1)))
+  }, [])
+  
   return theme
 }
