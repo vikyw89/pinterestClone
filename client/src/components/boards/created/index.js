d00cee0af86124f342bb3d43b4f920f5e805d1aa
vikyw89 <vikyw89@gmail.com>
2023-05-21 13:08:20 +0800

    done on created Pins n /profile

--- client/src/components/boards/created/index.js
+++ client/src/components/boards/created/index.js
@@ -1,7 +1,15 @@
+import { FeedsComponent } from "@/components/feed"
+import { useBoard } from "@/lib/hooks/useBoard"
+
 export const CreatedComponent = () => {
+  const { data: allCreatedPins } = useBoard('all pins')
+  console.log("🚀 ~ file: index.js:5 ~ CreatedComponent ~ allCreatedPins:", allCreatedPins)
+
   return (
-    <div>
-            Work in Progress
+    <div className="flex justify-center flex-1 items-center">
+      {allCreatedPins &&
+        < FeedsComponent props={{ feeds: allCreatedPins, infinite: false, refetchFn: () => undefined }} />
+      }
     </div>
   )
 }
