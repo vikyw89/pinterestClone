acda2aa01f490a1a736d7e757931d14287cc9e29
vikyw89 <vikyw89@gmail.com>
2023-05-23 15:33:58 +0800

    feat: better auth rerouting

--- client/src/lib/hooks/useAuth.js
+++ client/src/lib/hooks/useAuth.js
@@ -1,7 +1,13 @@
 import useSWRSubscription from 'swr/subscription'
 import { supabase } from '../supabase'
+import useSWRImmutable from 'swr/immutable'
 
 export const useAuth = () => {
+  const session = useSWRImmutable('api/auth', async () => {
+    const response = await supabase.auth.getUser()
+    const data = response?.data?.user
+    return data
+  }, { errorRetryCount: 0 })
   const auth = useSWRSubscription('api/sub/auth', (key, { next }) => {
     const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
       next(null, session)
@@ -10,5 +16,8 @@ export const useAuth = () => {
       subscription.unsubscribe()
     }
   })
-  return auth
+  return {
+    ...session,
+    ...auth
+  }
 }
