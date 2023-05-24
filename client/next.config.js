02523f332fecd76948c0abd43792a134003537dc
vikyw89 <vikyw89@gmail.com>
2023-05-24 13:16:01 +0800

    added experimental scroll restoration

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  experimental:{
+    scrollRestoration:true
+  },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   // assetPrefix: `${process.env.NEXT_PUBLIC_BASE_PATH}/`,
   images: {
