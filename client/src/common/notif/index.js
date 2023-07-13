a9ebe39e2ab689062e56bb6e976be8a233a0ac5b
vikyw89 <vikyw89@gmail.com>
2023-07-13 12:35:58 +0800

    fix: attempt#4 at removing duplicated notif

--- client/src/common/notif/index.js
+++ client/src/common/notif/index.js
@@ -1,5 +1,4 @@
 import { useSyncSWR } from 'swr-sync-state'
-import { v4 } from 'uuid'
 
 export const Notif = () => {
   const error = useSyncSWR('notif/error') ?? []
@@ -15,7 +14,7 @@ export const Notif = () => {
       {error &&
         error.map((e) => {
           return (
-            <div key={v4()} className="alert alert-error shadow-lg animate-bounce w-fit" onClick={popNotif}>
+            <div key={e} className="alert alert-error shadow-lg animate-bounce w-fit" onClick={popNotif}>
               <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                 <span>{e.message}</span>
@@ -27,7 +26,7 @@ export const Notif = () => {
       {warning &&
         warning.map((e) => {
           return (
-            <div key={v4()} className="alert alert-warning shadow-lg animate-bounce w-fit" onClick={popNotif}>
+            <div key={e} className="alert alert-warning shadow-lg animate-bounce w-fit" onClick={popNotif}>
               <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                 <span>{e}</span>
@@ -39,7 +38,7 @@ export const Notif = () => {
       {info &&
         info.map((e) => {
           return (
-            <div key={v4()} className="alert alert-info shadow-lg animate-bounce w-fit" onClick={popNotif}>
+            <div key={e} className="alert alert-info shadow-lg animate-bounce w-fit" onClick={popNotif}>
               <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                 <span>{e}</span>
@@ -51,7 +50,7 @@ export const Notif = () => {
       {success &&
         success.map((e) => {
           return (
-            <div key={v4()} className="alert alert-success shadow-lg animate-bounce w-fit" onClick={popNotif}>
+            <div key={e} className="alert alert-success shadow-lg animate-bounce w-fit" onClick={popNotif}>
               <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                 <span>{e}</span>
--- client/src/lib/hooks/useAuth.js
+++ client/src/lib/hooks/useAuth.js
@@ -19,10 +19,12 @@ export const useAuth = () => {
           }
           // prevent duplicate notif
           const message = `Welcome aboard, ${name} !`
+          
+          if (p.slice(-1) === message) return p
           setTimeout(() => {
-            setSyncSWR('notif/info', [])
+            setSyncSWR('notif/info', p => p.slice(1))
           }, 5000)
-          return [message]
+          return [...p, message]
         })
       } else {
         setSyncSWR('notif/info', p => {
@@ -31,10 +33,11 @@ export const useAuth = () => {
           }
           // prevent duplicate notif
           const message = 'You\'re now signed out !'
+          if (p.slice(-1) === message) return p
           setTimeout(() => {
             setSyncSWR('notif/info', p => p.slice(1))
           }, 5000)
-          return [message]
+          return [...p, message]
         })
       }
       next(null, session)
